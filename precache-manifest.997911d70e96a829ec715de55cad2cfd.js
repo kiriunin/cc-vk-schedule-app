07281c2bfd6d273c30e8ff87fd3e4a494c296e73
kiriunin <e@kiriunin.ru>
2019-06-22 18:01:58 +0300

    Updates

---
+++ precache-manifest.997911d70e96a829ec715de55cad2cfd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "2f9372ded94acf8e7007",
+    "url": "/cc-vk-schedule-app/static/css/main.02080376.chunk.css"
+  },
+  {
+    "revision": "2f9372ded94acf8e7007",
+    "url": "/cc-vk-schedule-app/static/js/main.4cdc9549.chunk.js"
+  },
+  {
+    "revision": "ae52b544b0d910475772",
+    "url": "/cc-vk-schedule-app/static/js/runtime~main.21d70920.js"
+  },
+  {
+    "revision": "b4460e3777da8e47ab7f",
+    "url": "/cc-vk-schedule-app/static/css/2.9d28c6b2.chunk.css"
+  },
+  {
+    "revision": "b4460e3777da8e47ab7f",
+    "url": "/cc-vk-schedule-app/static/js/2.bf32bd0e.chunk.js"
+  },
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "/cc-vk-schedule-app/static/media/persik.4e1ec840.png"
+  },
+  {
+    "revision": "22573010f2d65caa4d43af3fd6eeab15",
+    "url": "/cc-vk-schedule-app/index.html"
+  }
+];
